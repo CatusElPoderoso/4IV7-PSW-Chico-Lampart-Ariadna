f39b613399c9a4b229a3e7745ea61c49c0eb51e4
Ariadna <achicol1900@alumno.ipn.mx>
2021-04-27 16:38:55 -0500

    frontend

---
+++ ServletHolaMundo/web/JS/formulario.js
@@ -0,0 +1,39 @@
+
+function caracteres(){
+    // sólo se aceptan letras mayúsculas y minúsculas. No espacios ni acentos.
+    var letras = /^[A-Za-z]+$/;
+
+    if(document.querySelector('#nombre').value.match(letras) && document.querySelector('#appat').value.match(letras) &&
+    document.querySelector('#appmat').value.match(letras)){
+        console.log('solo hay letras');
+        return true;
+    }
+        if(document.querySelector('#nombre').value.trim()=="" || document.querySelector('#appat').value.trim()=="" ||
+            document.querySelector('#appmat').value.trim()==""){
+            console.log('hay espacios vacíos en registrar usuario')
+            alert('Por favor, complete todos los espacios')
+            event.preventDefault()
+        }else{
+                console.log('se trataron de ingresar otros caracteres');
+                alert('Por favor, ingrese únicamente letras (sin acentos ni espacios)');
+                event.preventDefault()
+            }       
+}
+function numeros(){
+    // solo se aceptan numeros sin espacios
+    var numeros = /^[0-9]+$/;
+
+    if(document.querySelector('#eliminar').value.match(numeros) && document.querySelector('#eliminar').value.trim() !== ""){
+        console.log('solo hay números');
+        return true;
+    }
+        if(document.querySelector('#eliminar').value.trim() == ""){
+            console.log('hay espacios vacíos en borrar usuario')
+            alert('Por favor, ingrese el ID del usuario que desea eliminar')
+            event.preventDefault()
+        }else{
+                console.log('se trataron de ingresar caracteres distintos a números');
+                alert('Por favor, ingrese únicamente números para eliminar un usuario');
+                event.preventDefault()
+            }
+}
