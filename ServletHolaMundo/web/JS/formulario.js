eee8acf69652bbe628c0046122ba2225f711f142
Ariadna <achicol1900@alumno.ipn.mx>
2021-04-28 12:55:45 -0500

    validación completa

--- ServletHolaMundo/web/JS/formulario.js
+++ ServletHolaMundo/web/JS/formulario.js
@@ -65,27 +65,35 @@ function cambiar(){
 
     if(document.querySelector('#actualizar').value.match(numeros) && document.querySelector('#actualizar').value.trim()!==""){
         console.log('el ID tiene números y no está vacío');
-        return true;
-    }
-        if(document.querySelector('#actualizar').value.trim()==""){
-            console.log('el ID está vacío');
-            alert('Por favor, ingrese un ID válido')
-            event.preventDefault()
-        }
-            if(document.querySelector('#nombre_2').value.match(letras) && document.querySelector('#appat_2').value.match(letras) &&
-            document.querySelector('#appmat_2').value.match(letras) && document.querySelector('#edad_2').value.match(letras) &&
+
+        if(document.querySelector('#nombre_2').value.trim()=="" && document.querySelector('#appat_2').value.trim()=="" &&
+            document.querySelector('#appmat_2').value.trim()=="" && document.querySelector('#edad_2').value.trim()=="" &&
+            document.querySelector('#email_2').value.trim()==""){
+
+                console.log('Todos los campos están vacíos')
+                alert('Todos los campos están vacíos, no se realizará ningún cambio')
+                event.preventDefault()
+
+            }
+        if(document.querySelector('#nombre_2').value.match(letras) || document.querySelector('#appat_2').value.match(letras) ||
+            document.querySelector('#appmat_2').value.match(letras) || document.querySelector('#edad_2').value.match(letras) ||
             document.querySelector('#email_2').value.match(letras)){
-                console.log('solo hay letras en los campos de actualizar usuario');
-            }   
-                if(document.querySelector('#nombre_2').value.trim()=="" && document.querySelector('#appat_2').value.trim()=="" &&
-                document.querySelector('#appmat_2').value.trim()=="" && document.querySelector('#edad_2').value.trim()=="" &&
-                document.querySelector('#email_2').value.trim()==""){
-                    console.log('Todos los campos están vacíos')
-                    alert('Todos los campos están vacíos, no se realizará ningún cambio')
-                    event.preventDefault();
-                }else{
-                    console.log('se trataron de ingresar otros caracteres');
-                    alert('Por favor, ingrese únicamente letras (sin acentos ni espacios)');
-                    event.preventDefault();
-                }       
+
+                console.log('algun campo contiene letras');
+
+            }else{
+
+                console.log('se trataron de ingresar otros caracteres');
+                alert('Por favor, ingrese únicamente letras (sin acentos ni espacios)');
+                event.preventDefault()
+                
+            }
+
+    }else{
+
+        console.log('el ID está vacío');
+        alert('Por favor, ingrese un ID válido')
+        event.preventDefault()
+
+    }         
 }
